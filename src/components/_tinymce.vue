222a59184dea25d07affabc21f4f31361a2f6fa7
David Monterde <dmonterde@cuixware.com>
2019-03-07 22:59:33 -0600

    Version funcional de vue con tinymce 5.0.2

---
+++ src/components/_tinymce.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Editor</h1>
+        <textarea id="textarea-tinymce" ref="textarea-tinymce"></textarea>
+    </div>
+</template>
+
+
+<script>
+    import tinymce from 'tinymce'
+
+    // A theme is also required
+    import esMX from '@/static/tinymce/langs/es_MX.js';
+    import '@/static/tinymce/themes/silver/theme.min.js';
+
+    import '@/static/tinymce/skins/ui/oxide/skin.min.css';
+    
+    import '@/static/tinymce/plugins/paste/plugin.min.js';
+    import '@/static/tinymce/plugins/link/plugin.min.js';
+    import '@/static/tinymce/plugins/autoresize/plugin.min.js';
+    import '@/static/tinymce/plugins/pagebreak/plugin.min.js';
+    import '@/static/tinymce/plugins/preview/plugin.min.js';
+    import '@/static/tinymce/plugins/print/plugin.min.js';
+    import '@/static/tinymce/plugins/table/plugin.min.js';
+
+
+    export default {
+        created(){
+            tinymce.addI18n('es_MX', esMX)
+            
+            tinymce.init({
+                selector: 'textarea#textarea-tinymce',
+                language: 'es_MX',
+                plugins: ['paste', 'table', 'print', 'link', 'autoresize', 'pagebreak', 'preview']
+            });
+        }
+        
+    }
+</script>
---
+++ src/views/Editor.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <editor-mce></editor-mce>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EditorMce from '@/components/_tinymce.vue'
+
+export default {
+  name: 'home',
+  components: {
+    EditorMce
+  }
+}
+</script>
